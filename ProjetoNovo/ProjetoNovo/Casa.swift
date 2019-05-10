2c2056c39f15456ceb323498c4bfb1890fb3b91e
Digital House <digitalhouse@DH-A1-10.local>
2019-05-09 21:31:17 -0300

    Cria Classe Casa

---
+++ ProjetoNovo/ProjetoNovo/Casa.swift
@@ -0,0 +1,13 @@
+//
+//  Casa.swift
+//  ProjetoNovo
+//
+//  Created by Digital House on 09/05/19.
+//  Copyright © 2019 Digital House. All rights reserved.
+//
+
+import Foundation
+
+class Casa {
+    var corPorta: String = "azul"
+}
